b7256962dae562bc78937409715ba25ff8e278a3
Evgeniy Kislov <evgeniykislov12@gmail.com>
2020-11-19 12:45:08 +0300

    Fixed 2.0

--- task3/main.cpp
+++ task3/main.cpp
@@ -12,80 +12,28 @@
 
 using namespace std;
 
-void parseCommand(int argc, char** argv, int& first, int& last) {
-    if (argc != 3) {
-        cout << "2 parameters required" << endl;
-        exit(0);
-    }
-
-    first = atoi(argv[1]);
-    last = atoi(argv[2]);
-}
-
-void printInfo(double time, double maxTime, int result, int first, int last) {
-    cout << "Total time: " << time << endl;
-    cout << "Max time among processes: " << maxTime << endl;
-    cout << result << " prime numbers between " << first << " and " << last << endl;
-}
-
-void toFile(int first, int* all, int len, string fileName) {
-    ofstream file;
-    
-    file.open(fileName.c_str(), ios::out | ios::trunc);
-
-    for (int i = 0; i < len; ++i) {
-        if (all[i]) {
-            file << first + i << " ";
-        }
-    }
-    
-    file.close();
-}
-
-void timeToCSV(string fileName, double time, int procNum) {
-    ofstream file;
-    file.open(fileName.c_str(), ios::out | ios::app);
-
-    file << procNum << "\t" << time << endl;
-
-    file.close();
-}
-
-void init(int& procNum, int& procRank, int*& all, int first, int last) {
-    MPI_Comm_size(MPI_COMM_WORLD, &procNum);
-    MPI_Comm_rank(MPI_COMM_WORLD, &procRank);
-
-    if (procRank == 0) {
-        cout << endl << "Number of processes: " << procNum << endl << endl;
-        all = new int [last - first];
-    }
-
-    MPI_Bcast(&first, 1, MPI_INT, 0, MPI_COMM_WORLD);
-    MPI_Bcast(&last, 1, MPI_INT, 0, MPI_COMM_WORLD);
-}
-
 void primeNumbers(int* all, int first, int last, int procNum, int procRank) {
     int len = last - first;
     int iMax = int(sqrt(last));
-    int* primeHelp = new int [iMax + 1];
+    int* trunkedPrimes = new int [iMax + 1];
 
-    primeHelp[0] = 0;
-    primeHelp[1] = 0;
+    trunkedPrimes[0] = 0;
+    trunkedPrimes[1] = 0;
     for (int i = 2; i <= iMax; ++i) {
-        primeHelp[i] = 1;
+        trunkedPrimes[i] = 1;
     }
 
     for (int i = 2; i <= iMax; ++i) {
-        if (primeHelp[i]) {
+        if (trunkedPrimes[i]) {
             for (int j = i*i; j <= iMax; j += i) {
-                primeHelp[j] = 0;
+                trunkedPrimes[j] = 0;
             }
         }
     }
 
     int partSize = ceil(len * 1.0 / procNum);
-    int firstElem = procRank * partSize;
-    int lastElem = procRank == procNum - 1 ? len - 1 : firstElem + partSize - 1;
+    int firstElement = procRank * partSize;
+    int lastElement = procRank == procNum - 1 ? len - 1 : firstElement + partSize - 1;
     int buf[partSize];
 
     for (int i = 0; i < partSize; ++i) {
@@ -93,11 +41,11 @@ void primeNumbers(int* all, int first, int last, int procNum, int procRank) {
     }
 
     for (int i = 2; i <= iMax; ++i) {
-        if (primeHelp[i]) {
-            int a = ceil((firstElem + first - i*i) * 1.0 / i);
+        if (trunkedPrimes[i]) {
+            int a = ceil((firstElement + first - i*i) * 1.0 / i);
             a = a < 0 ? 0 : a;
-            for (int j = i*i + a * i; j <= lastElem + first; j += i) {
-                buf[j - firstElem - first] = 0;
+            for (int j = i*i + a * i; j <= lastElement + first; j += i) {
+                buf[j - firstElement - first] = 0;
             }
         }
     }
@@ -116,8 +64,24 @@ int main(int argc, char** argv) {
 
     MPI_Init(&argc, &argv);
 
-    parseCommand(argc, argv, first, last);
-    init(procNum, procRank, all, first, last);
+    if (argc != 3) {
+        cout << "2 parameters required" << endl;
+        exit(0);
+    }
+
+    first = atoi(argv[1]);
+    last = atoi(argv[2]);
+
+    MPI_Comm_size(MPI_COMM_WORLD, &procNum);
+    MPI_Comm_rank(MPI_COMM_WORLD, &procRank);
+
+    if (procRank == 0) {
+        cout << endl << "Number of processes: " << procNum << endl << endl;
+        all = new int [last - first];
+    }
+
+    MPI_Bcast(&first, 1, MPI_INT, 0, MPI_COMM_WORLD);
+    MPI_Bcast(&last, 1, MPI_INT, 0, MPI_COMM_WORLD);
 
     begin = MPI_Wtime();
     primeNumbers(all, first, last, procNum, procRank);
