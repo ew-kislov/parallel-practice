0c94c3d8cd3fd3aa4829adedf17d5394ac7b26e6
Evgeniy Kislov <evgeniykislov12@gmail.com>
2020-11-26 13:33:30 +0300

    Refactoring 4.0

--- task_4/main.cpp
+++ task_4/main.cpp
@@ -30,8 +30,8 @@ double* matrixFromBin(string fileName, int& rows, int& cols, int& mode) {
 
     matrix = new double [rows * cols];
 
-    for (int i = 0; i < rows; ++i) {
-        for (int j = 0; j < cols; ++j) {
+    for (int i = 0; i < rows; i++) {
+        for (int j = 0; j < cols; j++) {
             if (mode == 1) {
                 read(fd, &(matrix[i * cols + j]), sizeof(double));
             } else {
@@ -61,7 +61,7 @@ double* vectorFromBin(string fileName, int& length) {
 }
 
 void printVector(double* vector, int rows) {
-    for (int i = 0; i < rows; ++i) {
+    for (int i = 0; i < rows; i++) {
         cout << vector[i] << " ";
     }
     cout << endl << endl;
@@ -88,7 +88,7 @@ int* getBounds(int size, int rank, int procNum) {
     return bounds;
 }
 
-double* multParallelColumns(double* matrix, double* vector, int rows, int cols, int procRank, int procNum) {
+double* multiplyParallelColumns(double* matrix, double* vector, int rows, int cols, int procRank, int procNum) {
     int lowBound, upperBound;
     double *result, *buffResult, *buffVector, *buffCols;
     MPI_Status status;
@@ -102,10 +102,10 @@ double* multParallelColumns(double* matrix, double* vector, int rows, int cols, 
         result = new double [rows];
         for (int i = 1; i < procNum; i++) {
             int* iBounds = getBounds(cols, i, procNum);
-            int iLowBound = iBounds[0];
-            int iUpperBound = iBounds[1];
-            MPI_Isend(&matrix[iLowBound * rows], (iUpperBound - iLowBound) * rows, MPI_DOUBLE, i, MASTER_TO_SLAVE_TAG, MPI_COMM_WORLD, &request);
-            MPI_Isend(&vector[iLowBound], iUpperBound - iLowBound, MPI_DOUBLE, i, MASTER_TO_SLAVE_TAG + 1, MPI_COMM_WORLD, &request);
+            int iterLowBound = iBounds[0];
+            int iterUpperBound = iBounds[1];
+            MPI_Isend(&matrix[iterLowBound * rows], (iterUpperBound - iterLowBound) * rows, MPI_DOUBLE, i, MASTER_TO_SLAVE_TAG, MPI_COMM_WORLD, &request);
+            MPI_Isend(&vector[iterLowBound], iterUpperBound - iterLowBound, MPI_DOUBLE, i, MASTER_TO_SLAVE_TAG + 1, MPI_COMM_WORLD, &request);
             delete[] iBounds;
         }
     }
@@ -121,12 +121,12 @@ double* multParallelColumns(double* matrix, double* vector, int rows, int cols, 
     }
 
     buffResult = new double [rows];
-    for (int i = 0; i < rows; ++i) {
+    for (int i = 0; i < rows; i++) {
         buffResult[i] = 0;
     }
 
     for (int i = 0; i < upperBound - lowBound; i++) {
-        for (int j = 0; j < rows; ++j) {
+        for (int j = 0; j < rows; j++) {
             buffResult[j] += buffCols[i * rows + j] * buffVector[i];
         }
     }
@@ -146,7 +146,7 @@ double* multParallelColumns(double* matrix, double* vector, int rows, int cols, 
     return NULL;
 }
 
-double* multParallelRows(double* matrix, double* vector, int rows, int cols, int procRank, int procNum) {
+double* multiplyParallelRows(double* matrix, double* vector, int rows, int cols, int procRank, int procNum) {
     int lowBound, upperBound;
     double *result, *buffResult, *buffRows;
     MPI_Status status;
@@ -166,9 +166,9 @@ double* multParallelRows(double* matrix, double* vector, int rows, int cols, int
         result = new double [rows];
         for (int i = 1; i < procNum; i++) {
             int* iBounds = getBounds(rows, i, procNum);
-            int iLowBound = iBounds[0];
-            int iUpperBound = iBounds[1];
-            MPI_Isend(&matrix[iLowBound * cols], (iUpperBound - iLowBound) * cols, MPI_DOUBLE, i, MASTER_TO_SLAVE_TAG, MPI_COMM_WORLD, &request);
+            int iterLowBound = iBounds[0];
+            int iterUpperBound = iBounds[1];
+            MPI_Isend(&matrix[iterLowBound * cols], (iterUpperBound - iterLowBound) * cols, MPI_DOUBLE, i, MASTER_TO_SLAVE_TAG, MPI_COMM_WORLD, &request);
 
             delete[] iBounds;
         }
@@ -185,7 +185,7 @@ double* multParallelRows(double* matrix, double* vector, int rows, int cols, int
 
     for (int i = 0; i < upperBound - lowBound; i++) {
         buffResult[i] = 0;
-        for (int j = 0; j < cols; ++j) {
+        for (int j = 0; j < cols; j++) {
             buffResult[i] += buffRows[i * cols + j] * vector[j];
         }
     }
@@ -199,9 +199,9 @@ double* multParallelRows(double* matrix, double* vector, int rows, int cols, int
     if (procRank == 0) {
         for (int i = 1; i < procNum; i++) {
             int* iBounds = getBounds(rows, i, procNum);
-            int iLowBound = iBounds[0];
-            int iUpperBound = iBounds[1];
-            MPI_Recv(&result[iLowBound], iUpperBound - iLowBound, MPI_DOUBLE, i, SLAVE_TO_MASTER_TAG, MPI_COMM_WORLD, &status);
+            int iterLowBound = iBounds[0];
+            int iterUpperBound = iBounds[1];
+            MPI_Recv(&result[iterLowBound], iterUpperBound - iterLowBound, MPI_DOUBLE, i, SLAVE_TO_MASTER_TAG, MPI_COMM_WORLD, &status);
 
             delete[] iBounds;
         }
@@ -216,10 +216,12 @@ double* multParallelRows(double* matrix, double* vector, int rows, int cols, int
 
 int main(int argc, char *argv[]) {
     int procRank, procNum, strategy;
+
     string fileMatrix = "data/matrix.bin";
     string fileVector = "data/vector.bin";
     string fileResult = "data/result.bin";
     string fileTime = "time.csv";
+
     double *vector, *result, *matrix;
     int rows, cols;
     double start, time, maxTime;
@@ -231,13 +233,7 @@ int main(int argc, char *argv[]) {
     if (procRank == 0) {
         matrix = matrixFromBin(fileMatrix, rows, cols, strategy);
         vector = vectorFromBin(fileVector, cols);
-
-        cout << rows << " x " << cols << endl;
-        if (strategy == 1) {
-            cout << "Parallel by rows" << endl;
-        } else {
-            cout << "Parallel by columns" << endl;
-        }
+        cout << rows << "x" << cols << endl;
     }
 
     MPI_Bcast(&strategy, 1, MPI_INT, 0, MPI_COMM_WORLD);
@@ -246,9 +242,9 @@ int main(int argc, char *argv[]) {
 
     start = MPI_Wtime();
     if (strategy == 1) {
-        result = multParallelRows(matrix, vector, rows, cols, procRank, procNum);
+        result = multiplyParallelRows(matrix, vector, rows, cols, procRank, procNum);
     } else {
-        result = multParallelColumns(matrix, vector, rows, cols, procRank, procNum);
+        result = multiplyParallelColumns(matrix, vector, rows, cols, procRank, procNum);
     }
     time = MPI_Wtime() - start;
 
@@ -267,6 +263,5 @@ int main(int argc, char *argv[]) {
         delete[] vector;
         delete[] result;
     }
-
     return 0;
 }
