4694fcae1301c3429940a167ec26e56c1b7fa9d0
Evgeniy Kislov <evgeniykislov12@gmail.com>
2020-12-10 20:24:11 +0300

    task 5 finished

--- task_5/main.cpp
+++ task_5/main.cpp
@@ -7,6 +7,7 @@
 #include <fcntl.h>
 #include <unistd.h>
 #include <string>
+#include <tuple>
 #include <math.h>
 #include <tgmath.h>
 
@@ -15,131 +16,110 @@
 
 using namespace std;
 
-int coordsToRank(int* coords, int size) {
-    return size * (size * coords[2] + coords[0]) + coords[1];
-}
-
-int coordsToRank(int i, int j, int k, int size) {
-    return size * (size * k + i) + j;
-}
-
-int* getProcCoords(int procRank, int procNum) {
-    int blockSize = int(cbrt(procNum));
-    int* coords = new int[3];
-
-    coords[0] = (procRank / blockSize) % blockSize;
-    coords[1] = procRank % blockSize;
-    coords[2] = (procRank / int(pow(blockSize, 2))) % blockSize;
-
-    return coords;
-}
-
-int coordsToOffset(int* coords, int n, int procNum) {
-    int blockNum = int(cbrt(procNum));
-    int blockElems = n / blockNum;
-    return coords[0] * n * blockElems + coords[1] * blockElems;
-}
-
-int sendMatrix(int* procCoords, int procRank, int procNum) {
-    int di, dj, dk;
-    int si, sj, sk;
-
-    if (procCoords[2] == 0 && procCoords[1] != procCoords[2]) {
-        di = procCoords[0];
-        dj = procCoords[1];
-        dk = procCoords[1];
-
-        int destRank = coordsToRank(di, dj, dk, blockNum);
-
-        MPI_Send(A, blockElems * blockElems, MPI_DOUBLE, destRank, MASTER_TO_SLAVE_TAG, MPI_COMM_WORLD);
+void sendMatrix(int* coordinates, int procNum, int blockSize, int procRank) {
+    if (coordinates[2] == 0 && coordinates[1] != coordinates[2]) {
+        int* newBlockCoordinates = setCoordinates(coordinates[0], coordinates[1], coordinates[1]);
+        int destRank = coordinatesToRank(newBlockCoordinates, blockNum);
+        MPI_Send(A, blockSize * blockSize, MPI_DOUBLE, destRank, MASTER_TO_SLAVE_TAG, MPI_COMM_WORLD);
     }
 
-    if (procCoords[1] != 0 && procCoords[1] == procCoords[2]) {
-        si = procCoords[0];
-        sj = procCoords[1];
-        sk = 0;
-
-        int srcRank = coordsToRank(si, sj, sk, blockNum);
-
-        MPI_Recv(A, blockElems * blockElems, MPI_DOUBLE, srcRank, MASTER_TO_SLAVE_TAG, MPI_COMM_WORLD, &status);
+    if (coordinates[1] && coordinates[1] == coordinates[2]) {
+        int* srcBlockCoordinates = setCoordinates(coordinates[0], coordinates[1], 0);
+        int srcRank = coordinatesToRank(srcBlockCoordinates, blockNum);
+        MPI_Recv(A, blockSize * blockSize, MPI_DOUBLE, srcRank, MASTER_TO_SLAVE_TAG, MPI_COMM_WORLD, &status);
     }
 
-    if (procCoords[1] == procCoords[2]) {
+    if (coordinates[1] == coordinates[2]) {
         for (int j = 0; j < blockNum; ++j) {
-            if (j != procCoords[2]) {
-                di = procCoords[0];
-                dj = j;
-                dk = procCoords[1];
-                int destRank = coordsToRank(di, dj, dk, blockNum);
-                MPI_Send(A, blockElems * blockElems, MPI_DOUBLE, destRank, MASTER_TO_SLAVE_TAG, MPI_COMM_WORLD);
+            if (j != coordinates[2]) {
+                int* newBlockCoordinates = setCoordinates(coordinates[0], j, coordinates[1]);
+                int destRank = coordinatesToRank(newBlockCoordinates, blockNum);
+                MPI_Send(A, blockSize * blockSize, MPI_DOUBLE, destRank, MASTER_TO_SLAVE_TAG, MPI_COMM_WORLD);
             }
         }
     } else {
-        si = procCoords[0];
-        sj = procCoords[2];
-        sk = procCoords[2];
-        int srcRank = coordsToRank(si, sj, sk, blockNum);
-        MPI_Recv(A, blockElems * blockElems, MPI_DOUBLE, srcRank, MASTER_TO_SLAVE_TAG, MPI_COMM_WORLD, &status);
+        int* srcBlockCoordinates = setCoordinates(coordinates[0], coordinates[2], coordinates[2]);
+        int srcRank = coordinatesToRank(srcBlockCoordinates, blockNum);
+        MPI_Recv(A, blockSize * blockSize, MPI_DOUBLE, srcRank, MASTER_TO_SLAVE_TAG, MPI_COMM_WORLD, &status);
     }
 
-    if (procCoords[2] == 0 && procCoords[0] != procCoords[2]) {
-        di = procCoords[0];
-        dj = procCoords[1];
-        dk = procCoords[0];
-        int destRank = coordsToRank(di, dj, dk, blockNum);
-        MPI_Send(B, blockElems * blockElems, MPI_DOUBLE, destRank, MASTER_TO_SLAVE_TAG + 1, MPI_COMM_WORLD);
+    if (coordinates[2] == 0 && coordinates[0] != coordinates[2]) {
+        int* newBlockCoordinates = setCoordinates(coordinates[0], coordinates[1], coordinates[0]);
+        int destRank = coordinatesToRank(newBlockCoordinates, blockNum);
+        MPI_Send(B, blockSize * blockSize, MPI_DOUBLE, destRank, MASTER_TO_SLAVE_TAG + 1, MPI_COMM_WORLD);
     }
 
-    if (procCoords[2] != 0 && procCoords[0] == procCoords[2]) {
-        si = procCoords[0];
-        sj = procCoords[1];
-        sk = 0;
-        int srcRank = coordsToRank(si, sj, sk, blockNum);
-        MPI_Recv(B, blockElems * blockElems, MPI_DOUBLE, srcRank, MASTER_TO_SLAVE_TAG + 1, MPI_COMM_WORLD, &status);
+    if (coordinates[2] && coordinates[0] == coordinates[2]) {
+        int* srcBlockCoordinates = setCoordinates(coordinates[0], coordinates[1], 0);
+        int srcRank = coordinatesToRank(srcBlockCoordinates, blockNum);
+        MPI_Recv(B, blockSize * blockSize, MPI_DOUBLE, srcRank, MASTER_TO_SLAVE_TAG + 1, MPI_COMM_WORLD, &status);
     }
 
-    if (procCoords[0] == procCoords[2]) {
+    if (coordinates[0] == coordinates[2]) {
         for (int i = 0; i < blockNum; ++i) {
-            if (i != procCoords[0]) {
-                di = i;
-                dj = procCoords[1];
-                dk = procCoords[2];
-                int destRank = coordsToRank(di dj, dk, blockNum);
-                MPI_Send(B, blockElems * blockElems, MPI_DOUBLE, destRank, MASTER_TO_SLAVE_TAG, MPI_COMM_WORLD);
+            if (i != coordinates[0]) {
+                int* newBlockCoordinates = setCoordinates(i, coordinates[1], coordinates[2]);
+                int destRank = coordinatesToRank(newBlockCoordinates, blockNum);
+                MPI_Send(B, blockSize * blockSize, MPI_DOUBLE, destRank, MASTER_TO_SLAVE_TAG, MPI_COMM_WORLD);
             }
         }
     } else {
-        si = procCoords[2];
-        sj = procCoords[1];
-        sk = procCoords[2];
-        int srcRank = coordsToRank(si, sj, sk, blockNum);
-        MPI_Recv(B, blockElems * blockElems, MPI_DOUBLE, srcRank, MASTER_TO_SLAVE_TAG, MPI_COMM_WORLD, &status);
+        int* srcBlockCoordinates = setCoordinates(coordinates[2], coordinates[1], coordinates[2]);
+        int srcRank = coordinatesToRank(srcBlockCoordinates, blockNum);
+        MPI_Recv(B, blockSize * blockSize, MPI_DOUBLE, srcRank, MASTER_TO_SLAVE_TAG, MPI_COMM_WORLD, &status);
     }
 }
 
-double* fromBin(string fileName, int& n, int procRank, int procNum) {
-    double* block = NULL;
+int* getProcCoordinates(int procRank, int procNum) {
+    int blockSize = int(cbrt(procNum));
+    int* coordinates = new int[3];
 
+    coordinates[0] = (procRank / blockSize) % blockSize;
+    coordinates[1] = procRank % blockSize;
+    coordinates[2] = (procRank / int(pow(blockSize, 2))) % blockSize;
+
+    return coordinates;
+}
+
+int coordinatesToOffset(int* coordinates, int n, int procNum) {
     int blockNum = int(cbrt(procNum));
-    int* procCoords = getProcCoords(procRank, procNum);
+    int blockSize = n / blockNum;
+    return coordinates[0] * n * blockSize + coordinates[1] * blockSize;
+}
+
+int coordinatesToRank(int* coordinates, int size) {
+    return size * (size * coordinates[2] + coordinates[0]) + coordinates[1];
+}
+
+int* setCoordinates(int x, int y, int z) {
+    int* coordinates = new int [3];
+    coordinates[0] = x;
+    coordinates[1] = y;
+    coordinates[2] = z;
+    return coordinates;
+}
+
+double* matrixFromBin(string fileName, int& n, int procRank, int procNum) {
+    double* block = NULL;
+    int blockNum = int(cbrt(procNum));
+    int* coordinates = getProcCoordinates(procRank, procNum);
 
     MPI_File file;
     MPI_Status status;
-
     MPI_File_open(MPI_COMM_WORLD, fileName.c_str(), MPI_MODE_RDONLY, MPI_INFO_NULL, &file);
     MPI_File_set_view(file, 0, MPI_INT, MPI_INT, "native", MPI_INFO_NULL);
     MPI_File_read(file, &n, 1, MPI_INT, &status);
 
-    int blockElems = n / blockNum;
-    block = new double [blockElems * blockElems];
-    int offset = coordsToOffset(procCoords, n, procNum);
+    int blockSize = n / blockNum;
+    block = new double [blockSize * blockSize];
+    int offset = coordinatesToOffset(coordinates, n, procNum);
 
     MPI_File_set_view(file, sizeof(int) + offset * sizeof(double), MPI_DOUBLE, MPI_DOUBLE, "native", MPI_INFO_NULL);
 
-    if (procCoords[2] == 0) {
-        for (int i = 0; i < blockElems; ++i) {
-            for (int j = 0; j < blockElems; ++j) {
-                MPI_File_read(file, &block[i * blockElems + j], 1, MPI_DOUBLE, &status);
+    if (coordinates[2] == 0) {
+        for (int i = 0; i < blockSize; ++i) {
+            for (int j = 0; j < blockSize; ++j) {
+                MPI_File_read(file, &block[i * blockSize + j], 1, MPI_DOUBLE, &status);
             }
             int offset;
             MPI_File_get_position(file, &offset);
@@ -152,43 +132,96 @@ double* fromBin(string fileName, int& n, int procRank, int procNum) {
     return block;
 }
 
+void matrixToBin(string fileName, double* C, int n, int procRank, int procNum) {
+    double* block = NULL;
+    int blockNum = int(cbrt(procNum));
+    int blockSize = n / blockNum;
+    int* coordinates = getProcCoordinates(procRank, procNum);
+
+    MPI_File file;
+    MPI_Status status;
+    MPI_File_open(MPI_COMM_WORLD, fileName.c_str(), MPI_MODE_WRONLY | MPI_MODE_CREATE, MPI_INFO_NULL, &file);
+    MPI_File_set_view(file, 0, MPI_INT, MPI_INT, "native", MPI_INFO_NULL);
+    MPI_File_write(file, &n, 1, MPI_INT, &status);
+
+    int offset = coordinatesToOffset(coordinates, n, procNum);
+    MPI_File_set_view(file, sizeof(int) + offset * sizeof(double), MPI_DOUBLE, MPI_DOUBLE, "native", MPI_INFO_NULL);
+
+    if (coordinates[2] == 0) {
+        for (int i = 0; i < blockSize; ++i) {
+            for (int j = 0; j < blockSize; ++j) {
+                MPI_File_write(file, &C[i * blockSize + j], 1, MPI_DOUBLE, &status);
+            }
+            MPI_File_seek(file, n - blockSize, MPI_SEEK_CUR);
+        }
+    }
+
+    MPI_File_close(&file);
+}
+
 int main(int argc, char* argv[]) {
     MPI_Status status;
-    int procRank, procNum, n;
+    int procRank;
+    int procNum;
+    int n;
 
-    double *A, *B, *C, *resultC;
+    double *A;
+    double *B;
+    double *C;
+    double *resultC;
 
     string fileA = "A.bin";
     string fileB = "B.bin";
+    string fileC = "C.bin";
 
     MPI_Init(&argc, &argv);
     MPI_Comm_rank(MPI_COMM_WORLD, &procRank);
     MPI_Comm_size(MPI_COMM_WORLD, &procNum);
 
-    A = fromBin(fileA, n, procRank, procNum);
-    B = fromBin(fileB, n, procRank, procNum);
+    A = matrixFromBin(fileA, n, procRank, procNum);
+    B = matrixFromBin(fileB, n, procRank, procNum);
 
     int blockNum = int(cbrt(procNum));
-    int blockElems = n / blockNum;
-    int* procCoords = getProcCoords(procRank, procNum);
+    int blockSize = n / blockNum;
+    int* coordinates = getProcCoordinates(procRank, procNum);
 
-    sendMatrix(procCoords, procRank, procNum);
+    sendMatrix(coordinates, procNum, blockSize, procRank);
 
-    C = new double [blockElems * blockElems];
-
-    for (int i = 0; i < blockElems; ++i) {
-        for (int j = 0; j < blockElems; ++j) {
-            C[i * blockElems + j] = 0;
+    C = new double [blockSize * blockSize];
+    for (int i = 0; i < blockSize; ++i) {
+        for (int j = 0; j < blockSize; ++j) {
+            C[i * blockSize + j] = 0;
         }
     }
 
-    for (int i = 0; i < blockElems; ++i) {
-        for (int j = 0; j < blockElems; ++j) {
-            for (int k = 0; k < blockElems; ++k) {
-                C[i * blockElems + j] += A[i * blockElems + k] * B[k * blockElems + j];
+    for (int i = 0; i < blockSize; ++i) {
+        for (int j = 0; j < blockSize; ++j) {
+            for (int k = 0; k < blockSize; ++k) {
+                C[i * blockSize + j] += A[i * blockSize + k] * B[k * blockSize + j];
             }
         }
     }
+
+    if (coordinates[2] == 0) {
+        resultC = new double [blockSize * blockSize];
+    }
+
+    int* rootCoordinates = setCoordinates(coordinates[0], coordinates[1], 0);
+    int root = coordinatesToRank(rootCoordinates, blockNum);
+
+    MPI_Comm MPI_COMM_VERTICAL;
+    int verticalRank, verticalNum;
+    int ijOffset = coordinates[0] * blockSize + coordinates[1];
+    
+    MPI_Comm_split(MPI_COMM_WORLD, ijOffset, procRank, &MPI_COMM_VERTICAL);
+    MPI_Comm_rank(MPI_COMM_VERTICAL, &verticalRank);
+    MPI_Comm_size(MPI_COMM_VERTICAL, &verticalNum);
+
+    MPI_Reduce(C, resultC, blockSize * blockSize, MPI_DOUBLE, MPI_SUM, 0, MPI_COMM_VERTICAL);
+    MPI_Comm_free(&MPI_COMM_VERTICAL);
+
+
+    matrixToBin(fileC, resultC, n, procRank, procNum);
 
     MPI_Finalize();
 }
